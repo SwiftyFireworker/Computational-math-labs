1857b16a9288f2c2baeafc7e687f248146fde1f1
SwiftyFireworker <78871040+SwiftyFireworker@users.noreply.github.com>
2021-10-25 23:08:38 +0300

    Update IV.12.8 b.cpp

--- IV.12.8 b.cpp
+++ IV.12.8 b.cpp
@@ -6,52 +6,80 @@
 //
 
 #include <iostream>
+#include <vector>
 #include <cmath>
 
 /*
+ IV.12.8
  f(x) = x * exp(-x^2), x >= 0
- epsilon = 1e-3
+ error <= 1e-3
+ Width on half of height - ?
  */
 
-double getFuncValue(double x) {
-    const double e = 2.71828;
-    double result = x * pow(e, -x * x);
-    return result;
+class Task {
+private:
+    static const double halfHeight;
+    
+    std::vector<double> roots = {};
+    
+public:
+    Task() { }
+    
+    static double fiFirst(const double &x) {
+        return (exp(x * x) * halfHeight);
+    }
+    
+    static double fiSecond(const double &x) {
+        return sqrt(log(x / halfHeight));
+    }
+    
+    double getDifferenceWithHalfHeight(double x) {
+        return (x * exp(-x * x) - halfHeight);
+    }
+    
+    void localizeRoots() {
+        double currentX = 0;
+        double edge = 2;
+        const double step = 1e-1;
+        
+        for( ; currentX < edge; currentX += step) {
+            if (getDifferenceWithHalfHeight(currentX) *
+                getDifferenceWithHalfHeight(currentX + step) <= 0) {
+                roots.push_back(currentX);
+            }
+        }
+    }
+    
+    double & getRootRef(const int i) {
+        return roots[i];
+    }
+    
+    double getWidth() {
+        return roots[1] - roots[0];
+    }
+};
+
+const double Task::halfHeight = 1 / (2 * sqrt(2 * M_E));
+
+
+bool useSimpleIterationMethod(double &x, double (*fi)(const double &)) {
+    const double epsilon = 1e-3;
+    while (fabs(fi(x) - x) > epsilon / 2) {
+        x = fi(x);
+    }
+    return true;
 }
 
+
 int main() {
-    /*
-     Working segment [0; 2]
-     On [0; 2] f'(x) <= 1, then Delta F ~ Delta X
-     */
-    const double step = 1e-3 / 4;
+    Task task = Task();
     
-    double currentX = 0;
-    double edge = 2;
+    task.localizeRoots();
     
-    double xHeight = 0;
-    double height = getFuncValue(xHeight);
+    useSimpleIterationMethod(task.getRootRef(0), Task::fiFirst);
+    useSimpleIterationMethod(task.getRootRef(1), Task::fiSecond);
     
-    while (currentX < edge) {
-        double currentValue = getFuncValue(currentX);
-        
-        if (currentValue > height) {
-            height = currentValue;
-            xHeight = currentX;
-        }
-        currentX += step;
-    }
-    
-    double x1 = xHeight, x2 = xHeight;
-    while (getFuncValue(x1) > height / 2) {
-        x1 -= step;
-    }
-    
-    while (getFuncValue(x2) > height / 2) {
-        x2 += step;
-    }
-    
-    std::cout << "Width: " << x2 - x1 << std::endl;
+    std::cout << "Width: " << task.getWidth() << std::endl;
     
     return 0;
 }
